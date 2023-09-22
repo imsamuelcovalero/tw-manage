ae3e745ab9c07d7a82181d3c0a41b5ae0a49bc21
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2023-09-22 15:17:20 -0300

    feat(Server e Rotas): Cria a estrutura base que permite acessar os endpoints de player e guilda.

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -2,11 +2,57 @@
 import React, { useEffect } from 'react'
 
 export default async function Home() {
+  function extractGuildId(url: string): string | null {
+    const match = url.match(/\/g\/([^\/]+)\//);
+    return match ? match[1] : null;
+  }
+
+  /** Create guild API link */
+  function getGuildApiLink(guildId: string) {
+    return `/api/guild?guildId=${guildId}`;
+  }
+
+  // Uso:
+  const url = "https://swgoh.gg/g/iGco7HmDSm6VbbIOxGoHuA/";
+  const guildId = extractGuildId(url);
+
+  if (!guildId) {
+    console.warn("Invalid URL provided. Unable to extract guild ID.");
+    return (
+      <div className="container">
+        <h1>Home</h1>
+        <p>Invalid guild URL provided.</p>
+      </div>
+    );
+  } else {
+    try {
+      const apiUrl = `http://localhost:3000${getGuildApiLink(guildId)}`;
+      console.log('apiUrl', apiUrl);
+
+      const response = await fetch(apiUrl);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      console.log("Guild Data:", data);
+      return (
+        <div className="container">
+          <h1>Home2</h1>
+          <p>{data.data.name}</p>
+        </div>
+      );
+    } catch (error: any) {
+      console.error("There was a problem with the fetch operation:", error.message);
+    }
+  }
+
   /** Create player API link */
   function getPlayerApiLink(allyCode: string) {
     const link = `/api/playerProxy?allyCode=${allyCode}`;
     // TODO: data check
-    console.log('link', link);
+    // console.log('link', link);
 
     return link;
   }
@@ -14,10 +60,10 @@ export default async function Home() {
   const allyCode = "417229579"
   try {
     const apiUrl = `http://localhost:3000${getPlayerApiLink(allyCode)}`;
-    console.log('apiUrl', apiUrl);
+    // console.log('apiUrl', apiUrl);
 
     const response = await fetch(apiUrl);
-    console.log('response', response);
+    // console.log('response', response);
 
 
     if (!response.ok) {
@@ -25,7 +71,7 @@ export default async function Home() {
     }
 
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
   } catch (error: any) {
     console.error("There was a problem with the fetch operation:", error.message);
   }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 /* File: src/server.ts */
 import express from 'express';
 import next from 'next';
-import httpProxy from 'http-proxy';
+// import httpProxy from 'http-proxy';
 import fetch from 'node-fetch'; // Importe o node-fetch no topo
 
 const port = parseInt(process.env.PORT || '3000', 10);
@@ -11,7 +11,8 @@ const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-const proxy = httpProxy.createProxy();
+// const proxy = httpProxy.createProxy();
+
 
 // Prepara a aplicação Next.js para manipulação
 nextApp.prepare().then(() => {
@@ -19,28 +20,80 @@ nextApp.prepare().then(() => {
 
   const app = express();
 
-  // Proxy para a API
-  app.get('/api/playerProxy', async (req, res) => {
-    const allyCode = req.query.allyCode as string;
-    console.log('allyCode', allyCode);
+  const BASE_API_URL = 'https://swgoh.gg/api/';
 
-    const targetUrl = `https://swgoh.gg/api/player/${allyCode}/`;
+  function constructUrl(endpoint: string): string {
+    return `${BASE_API_URL}${endpoint}`;
+  }
+
+  app.get('/api/guild', async (req, res) => {
+    const guildId = req.query.guildId as string;
+
+    if (!guildId) {
+      return res.status(400).send("Guild ID is required");
+    }
+
+    const targetUrl = constructUrl(`guild-profile/${guildId}/`);
 
     try {
-      // const fetch = (await import('node-fetch')).default;
       const response = await fetch(targetUrl);
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
       }
 
       const data = await response.json();
-      res.json(data); // Envie os dados para o cliente
-
+      res.json(data);
     } catch (error: any) {
       console.error("There was a problem with the fetch operation:", error.message);
-      res.status(500).send("Internal Server Error"); // Envie um erro ao cliente
+      res.status(500).send("Internal Server Error");
     }
   });
+
+  app.get('/api/player', async (req, res) => {
+    const allyCode = req.query.allyCode as string;
+
+    if (!allyCode) {
+      return res.status(400).send("AllyCode is required");
+    }
+
+    const targetUrl = constructUrl(`player/${allyCode}/`);
+
+    try {
+      const response = await fetch(targetUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      res.json(data);
+    } catch (error: any) {
+      console.error("There was a problem with the fetch operation:", error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+
+  // Proxy para a API
+  // app.get('/api/playerProxy', async (req, res) => {
+  //   const allyCode = req.query.allyCode as string;
+  //   console.log('allyCode', allyCode);
+
+  //   const targetUrl = `https://swgoh.gg/api/player/${allyCode}/`;
+
+  //   try {
+  //     // const fetch = (await import('node-fetch')).default;
+  //     const response = await fetch(targetUrl);
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`);
+  //     }
+
+  //     const data = await response.json();
+  //     res.json(data); // Envie os dados para o cliente
+
+  //   } catch (error: any) {
+  //     console.error("There was a problem with the fetch operation:", error.message);
+  //     res.status(500).send("Internal Server Error"); // Envie um erro ao cliente
+  //   }
+  // });
 
   // Manipula todos os outros pedidos com o Next.js
   app.all('*', (req, res) => {
